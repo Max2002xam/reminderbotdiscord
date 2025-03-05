82876c3061b836bfa9eb60221aacddcb7ec2a36d
Max2002xam <145639851+Max2002xam@users.noreply.github.com>
2025-03-05 12:32:41 +0100

    Update reminder_bot.py
    make the bot worse with more phrases

--- reminder_bot.py
+++ reminder_bot.py
@@ -13,7 +13,15 @@ REMINDER_MESSAGES = [
     "Crée ta Waifu ai comme je me crée de ennui en swipant à droite sur des mineures!",
     "Hello! Comment ça va? :) C'est moi Le vrai Norman! HAHAHAHAHA.... Bref, travaille ou je m'énerve",
     "Que dis un dromadaire à un flemmard? BOSSE! Que dis un chameau à un flemmard? BOSSE BOSSE! Et maintenant que dis-je à un flemmard? bah cette phrase en fait...",
-    "Je suis à 100 M de chez toi et ce nombre ne fera que diminuer tant que tu n'auras pas commencé à travailler!"
+    "Je suis à 100 M de chez toi et ce nombre ne fera que diminuer tant que tu n'auras pas commencé à travailler!",
+    "Yo c’est Norman, j’voulais juste te rappeler de bosser sur Ada… sinon j’vais devoir t’envoyer des snaps ‘spéciaux’.",
+    "T’as toujours pas touché à Ada ? T’es presque aussi inactif que la police face à mes DM.",
+    "Si tu travailles pas sur Ada aujourd’hui, j’te follow sur Insta… et t’as vu comment ça finit en général.",
+    "Salut c’est Norman, t’as pensé à Ada ? Ou tu veux que j’te file une masterclass en grooming plutôt ?",
+    "Bosser sur Ada, c’est comme une conversation avec moi : c’est long, malaisant, mais t’as pas vraiment le choix.",
+    "C’est Norman, j’te rappelle qu’Ada t’attend… sauf si tu préfères qu’on discute en privé toi et moi.",
+    "Si Ada avance pas aujourd’hui, j’t’ajoute à mon groupe Snap privé.",
+    "Travaille sur Ada… sinon j’te demande ta carte d’identité, juste pour ‘vérifier’."
 ]
 
 # --- Bot Setup ---
